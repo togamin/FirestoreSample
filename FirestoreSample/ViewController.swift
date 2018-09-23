d41b77e0075e429bccf8a900a6c3801f984f0c00
Togami Yuki <togaminnnn@gmail.com>
2018-09-23 09:05:29 +0800

    完了

--- FirestoreSample/ViewController.swift
+++ FirestoreSample/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var id:Int = 0
     var todoList:[String] = []
     var memoList:[String] = []
+    var idList:[String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,15 +40,28 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             } else {
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
-                    self.id += 1
+                    self.idList.append(document.documentID)
                     self.todoList.append(document.data()["ToDo"] as! String)
                     self.memoList.append(document.data()["memo"] as! String)
-                    print(self.todoList)
                 }
                 self.myTableView.reloadData()
             }
         }
         
+        //仮のサイズでツールバー生成
+        let kbToolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        kbToolBar.barStyle = UIBarStyle.default  // スタイルを設定
+        kbToolBar.sizeToFit()  // 画面幅に合わせてサイズを変更
+        // スペーサー
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
+        // 閉じるボタン
+        let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action:#selector(self.closeKeybord(_:)))
+        kbToolBar.items = [spacer, commitButton]
+        todoTextField.inputAccessoryView = kbToolBar
+        memoTextField.inputAccessoryView = kbToolBar
+    }
+    @objc func closeKeybord(_ sender:Any){
+        self.view.endEditing(true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -66,27 +80,60 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         //データの削除
         let deleteCell: UITableViewRowAction = UITableViewRowAction(style: .normal, title: "削除"){ (action, index) -> Void in
             
-            
-            
-            
+            self.defaultStore.collection("ToDoList").document(self.idList[indexPath.row]).delete() { err in
+                if let err = err {
+                    print("Error removing document: \(err)")
+                } else {
+                    print("Document successfully removed!")
+                }
+            }
         }
         let editCell: UITableViewRowAction = UITableViewRowAction(style: .normal, title: "編集"){ (action, index) -> Void in
             
-        
-            
-            
-            
+            //アラートの設定
+            let alert = UIAlertController(title: "データを編集します", message: nil, preferredStyle: .alert)
+            //OKボタン
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action:UIAlertAction!) -> Void in
+                //OKを押した後の処理。
+                self.defaultStore.collection("ToDoList").document(self.idList[indexPath.row]).setData([
+                    "ToDo": alert.textFields![0].text!,
+                    "memo": alert.textFields![1].text!
+                ]) { err in
+                    if let err = err {
+                        print("Error writing document: \(err)")
+                    } else {
+                        print("Document successfully written!")
+                    }
+                }
+                
+            }))
+            //キャンセルボタン
+            alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: {(action:UIAlertAction!) -> Void in
+                //キャンセルを押した後の処理。
+            }))
+            // テキストフィールドを追加。取り出し方は右「alert.textFields![0].text!」
+            alert.addTextField(configurationHandler: {(addTitleField: UITextField!) -> Void in
+                addTitleField.text = self.todoList[indexPath.row]
+                addTitleField.placeholder = "タイトルを入力してください。"//プレースホルダー
+            })
+            // テキストフィールドを追加。取り出し方は右「alert.textFields![1].text!」
+            alert.addTextField(configurationHandler: {(addTitleField: UITextField!) -> Void in
+                addTitleField.text = self.memoList[indexPath.row]
+                addTitleField.placeholder = "タイトルを入力してください。"//プレースホルダー
+            })
+            //その他アラートオプション
+            alert.view.layer.cornerRadius = 25 //角丸にする。
+            self.present(alert,animated: true,completion: {()->Void in print("アラート表示")})//completionは動作完了時に発動。
             
         }
-        deleteCell.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.6)
-        editCell.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.6)
+        deleteCell.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.8)
+        editCell.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 0.8)
         return [deleteCell,editCell]
     }
     
     //データベースにデータを追加する
     @IBAction func addData(_ sender: UIButton) {
-        self.id += 1
-        defaultStore.collection("ToDoList").document("\(id)").setData([
+        defaultStore.collection("ToDoList").addDocument(data:[
             "ToDo": todoTextField.text,
             "memo": memoTextField.text
         ]) { err in
@@ -96,6 +143,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
                 print("Document successfully written!")
             }
         }
+        
         //アラートの設定
         let alert = UIAlertController(title: "データを追加しました。", message: nil, preferredStyle: .alert)
         //OKボタン
@@ -104,22 +152,27 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }))
         //その他アラートオプション
         alert.view.layer.cornerRadius = 25 //角丸にする。
-        present(alert,animated: true,completion: {()->Void in print("アラート表示")})//completionは動作完了時に発動。
+        self.present(alert,animated: true,completion: {()->Void in print("アラート表示")})//completionは動作完了時に発動。
     }
     
     //データベースからデータを取り出す
     @IBAction func getData(_ sender: UIButton) {
+        todoList = []
+        memoList = []
+        idList = []
         defaultStore.collection("ToDoList").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
+                    self.idList.append(document.documentID)
+                    self.todoList.append(document.data()["ToDo"] as! String)
+                    self.memoList.append(document.data()["memo"] as! String)
                 }
                 self.myTableView.reloadData()
             }
         }
-        
     }
     
     
